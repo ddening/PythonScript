e58c745d864a14b2127c79ebee3ae21442b238c2
ddening <44156662+ddening@users.noreply.github.com>
2019-04-11 10:09:34 +0200

    __init__

---
+++ MergeSort/MergeSort.py
@@ -0,0 +1,17 @@
+def mergeSort():
+    """Merge Sort
+
+    Args:   
+        ...
+    """
+
+    # Code ...
+    string = "LOREM IPSUM"
+
+
+def main():
+    """Main Funktion"""
+    
+    # Code ...
+
+main()

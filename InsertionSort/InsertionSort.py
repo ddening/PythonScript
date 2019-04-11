9bbab6dfc453bd3400f4465dd1bf259e350cd940
ddening <44156662+ddening@users.noreply.github.com>
2019-04-11 08:04:24 +0200

    init

---
+++ InsertionSort/InsertionSort.py
@@ -0,0 +1,36 @@
+def insertion_sort(arr):
+    """Sortier ein Array mit InsertionSort 
+
+    Args:
+        arr: zu sortierendes Array
+    """
+
+    # Ausgabe vom Startarray
+    print("Unsortiertes Array:  ", arr)
+
+    # Durchlaufe jedes Element im Array
+    for i in range(1, len(arr)):
+        # zu vergleichender Wert
+        element = arr[i] 
+        # Index für Elemente links vom 'element'
+        index_left = i-1 
+        # solange der linke Wert größe als unser 'element' -- tausche deren Position
+        while index_left > -1 and arr[index_left] > element:
+            arr[index_left + 1] = arr[index_left]
+            arr[index_left] = element
+            index_left -= 1
+
+    # Ausgabe vom Array
+    print("Sortiertes Array:    ", arr)
+
+def main():
+    """Main Funktion"""
+
+    # Erstelle Array
+    myarr = [2, 1, 3, 17, 22, 21, 5239, 32, -12, 0, 2312, 231, 634, 2346, 19 , 9128, -5, 7] 
+    # Funktionsaufruf mit dem unsortierten Array
+    insertion_sort(myarr)
+
+    # print(insertion_sort.__doc__)
+
+main()
